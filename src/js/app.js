696042f431ab86c11eb73ca474ef96a8e158a0d9
Agata Szymeczko <agata.szymeczko21@gmail.com>
2024-12-17 18:07:06 +0100

    Add Booking class

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import { classNames, select, settings } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 export const app = {
   initPages: function () {
@@ -101,6 +102,7 @@ export const app = {
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 
   initCart: function () {
@@ -114,6 +116,12 @@ export const app = {
     thisApp.productList.addEventListener('add-to-cart', function (event) {
       app.cart.add(event.detail.product);
     });
+  },
+
+  initBooking: function () {
+    const thisApp = this;
+    const reservationWidget = document.querySelector(select.containerOf.booking);
+    thisApp.widget = new Booking(reservationWidget);
   },
 };
 
